cc841131d5a8f58232b45b02686927016b9c8b02
tanvi123123 <84211293+tanvi123123@users.noreply.github.com>
2021-05-18 08:04:47 -0400

    Update TopClass.java

--- TopClass.java
+++ TopClass.java
@@ -3,36 +3,37 @@ import javax.swing.JFrame;
 
 public class TopClass extends JFrame {
 
-    private static final int WIDTH_SCREEN = 800;
-    private static final int HEIGHT_SCREEN = 680;
+	private static final int WIDTH_SCREEN = 800;
+	private static final int HEIGHT_SCREEN = 680;
 
-    public TopClass() {
+	public TopClass() {
 
-        buildFrame();
-    }
-    /**
+		buildFrame();
+	}
+
+	/**
 	 * Method for constructing the JFrame and adding content
 	 */
-    private void buildFrame() {
+	private void buildFrame() {
 
-        this.add(new PlayGameScreen(WIDTH_SCREEN, HEIGHT_SCREEN));
-        this.pack();
-        this.setResizable(false);
-        this.setTitle("Coco's Adventure");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(WIDTH_SCREEN, HEIGHT_SCREEN);
-    }
+		this.add(new GameScreen(WIDTH_SCREEN, HEIGHT_SCREEN));
+		this.pack();
+		this.setResizable(false);
+		this.setTitle("Coco's Adventure");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(WIDTH_SCREEN, HEIGHT_SCREEN);
+	}
 
 	/**
 	 * Method invoked while jar file is running
 	 * 
 	 * @param args
 	 */
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        EventQueue.invokeLater(() -> {
-            TopClass tc = new TopClass();
-            tc.setVisible(true);
-        });
-    }
+		EventQueue.invokeLater(() -> {
+			TopClass tc = new TopClass();
+			tc.setVisible(true);
+		});
+	}
 }
